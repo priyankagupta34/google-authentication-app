111fa3531a96a6d63de44cc07e5efe37b47be69f
priyanka gupta <34priyankagupta@gmail.com>
2025-01-18 20:02:54 +0530

    --amend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
-import React, { useEffect } from "react";
-import { GoogleLogin, useGoogleLogin } from "@react-oauth/google";
+import React from "react";
+import { useGoogleOneTapLogin } from "@react-oauth/google";
 
 const App = () => {
-  const login = useGoogleLogin({
-    onSuccess: (credentialResponse) => {
-      const token = credentialResponse.credential;
-      console.log("Token:", token);
-    },
-    onError: () => {
-      console.error("Login Failed");
-    },
-    auto_select: true, // Automatically log in with the most recently authenticated user
-    cancel_on_tap_outside: false, // Keeps the prompt open even if the user clicks outside
-    context: "signin", // Can be 'signin' or 'signup'
-  });
-
-  useEffect(() => {
-    // Show the One Tap Login prompt
-    login();
-  }, [login]);
   const handleLoginSuccess = (credentialResponse) => {
     console.log("Login Success:", credentialResponse);
   };
@@ -27,16 +10,14 @@ const App = () => {
     console.error("Login Failed");
   };
 
+  useGoogleOneTapLogin({
+    onSuccess: handleLoginSuccess,
+    onError: handleLoginFailure,
+  });
+
   return (
     <div>
       <h1>Login with Google</h1>
-      <GoogleLogin
-        onSuccess={handleLoginSuccess}
-        onError={handleLoginFailure}
-        size="large"
-        useOneTap
-        width={200}
-      />
     </div>
   );
 };
