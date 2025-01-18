43f6921333fe540075bc8f2e4bfa2a9c3ec7e145
priyanka gupta <34priyankagupta@gmail.com>
2025-01-18 20:40:28 +0530

    --amend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { useGoogleOneTapLogin } from "@react-oauth/google";
 
 const App = () => {
-  const handleLoginSuccess = (credentialResponse) => {
-    console.log("Login Success:", credentialResponse);
+  const [user, setUser] = useState();
+  const handleLoginSuccess = async (response) => {
+    const idtoken = response.credential;
+    await getAccessToken(idtoken);
+  };
+
+  const getAccessToken = async (idToken) => {
+    try {
+      const response = await fetch(
+        `https://oauth2.googleapis.com/tokeninfo?id_token=${idToken}`
+      );
+      const data = await response.json();
+      if (data.error) {
+        console.error("Error fetching Access Token:", data.error);
+        return null;
+      }
+      setUser(data);
+      return data;
+    } catch (error) {
+      console.error("Error:", error);
+      return null;
+    }
   };
 
   const handleLoginFailure = () => {
@@ -15,9 +35,34 @@ const App = () => {
     onError: handleLoginFailure,
   });
 
+  const handleLogout = () => {
+    // Revoke the user session
+    window.google.accounts.id.revoke(user.email, () => {
+      console.log("User logged out");
+      setUser(null);
+      // Trigger One Tap again after logout
+      window.google.accounts.id.prompt(); // This will show the One Tap login screen again
+    });
+  };
+
   return (
     <div>
-      <h1>Login with Google</h1>
+      {!user ? (
+        <h1>Login with Google</h1>
+      ) : (
+        <nav className="menu">
+          <div className="flex">
+            <img src={user.picture} alt="profile" />
+            <div>{user.given_name}</div>
+          </div>
+
+          <div>
+            <button type="button" onClick={handleLogout}>
+              Logout
+            </button>
+          </div>
+        </nav>
+      )}
     </div>
   );
 };
